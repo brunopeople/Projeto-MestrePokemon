8706f205ed87f1ff49842315a5e6394d0b684bdb
brunopeople <brunopessoa.melo@gmail.com>
2019-10-23 17:09:18 -0300

    rotas e componentes criados, aplicação funcionando

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,51 @@
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+
 
 require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios);
+
+
+import HomeComponent from './components/HomeComponent.vue';
+import CreateComponent from './components/CreateComponent.vue';
+import IndexComponent from './components/IndexComponent.vue';
+import EditComponent from './components/EditComponent.vue';
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
+const routes = [
+{
+	name: 'home',
+	path: '/',
+	component: HomeComponent
+},
+
+{
+	name: 'create',
+	path: '/posts',
+	component: CreateComponent
+},
+
+{
+	name:'posts',
+	path: '/posts',
+	component: IndexComponent
+},
+
+{
+	name: 'edit',
+	path: '/edit/:id',
+	component: EditComponent
+	}
+	
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes});
+const app = new Vue(Vue.util.extend({router})).$mount('#app');
+
---
+++ resources/js/components/EditComponent.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="row-justify-content-center">
+		<div class="col-md-8">
+			<div class="card card-default">
+			  <div class="card-header">Create Component</div>
+
+			  <div class="card-body">
+			  	Eu sou o Create Component component. 
+			  </div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export deafult {
+		mounted(){
+			console.log('Component mouted.')
+		}
+	}
+</script>
